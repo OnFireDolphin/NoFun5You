f5eca29ed424f900721547e8ba5159607c611aaf
Alexander Vorobev <alexander.vorobev@polymtl.ca>
2021-02-06 14:36:25 -0500

    aa

---
+++ sample2/content.js
@@ -0,0 +1,19 @@
+//const { DH_CHECK_P_NOT_PRIME } = require('constants');
+const arr = ['You smell bad', 'Youtube is your enemy', 'Dont']
+const item = arr[Math.floor(Math.random() * arr.length)];
+
+alert(item);
+// alert('No youtube for you!');
+//chrome.runtime.onConnect(() => {
+//    alert('hi again');
+//});
+// chrome.tabs.getCurrent(function (tab) {
+//     chrome.tabs.remove(tab.id, function () {});
+// });
+// function closePage() {
+//     chrome.tabs.getCurrent(function (tab) {
+//         chrome.tabs.remove(tab.id, function () {});
+//     });
+// }
+
+// closePage();
